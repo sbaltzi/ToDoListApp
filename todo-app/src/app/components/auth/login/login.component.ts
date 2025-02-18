7ec6a05941db9044d6ebb375f1d089714b0659e8
sbaltzi <60263769+sbaltzi@users.noreply.github.com>
2025-02-18 12:12:32 +0200

    Update login page

--- todo-app/src/app/components/auth/login/login.component.ts
+++ todo-app/src/app/components/auth/login/login.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
+  standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  onLogin() {
+    const users = JSON.parse(localStorage.getItem('users') || '[]');
+    const user = users.find((u: any) => u.email === this.email && u.password === this.password);
+
+    if (user) {
+      localStorage.setItem('currentUser', JSON.stringify(user)); // Store logged-in user
+      alert('Login successful!');
+      this.router.navigate(['/todo-list']); // Redirect to Todo List
+    } else {
+      alert('Invalid email or password');
+    }
+  }
 }
