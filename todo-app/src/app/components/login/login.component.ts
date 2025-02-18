86733c48bb45bfa450670760596e41820adb9e20
sbaltzi <60263769+sbaltzi@users.noreply.github.com>
2025-02-18 18:24:39 +0200

    Disable login button for invalid form

--- todo-app/src/app/components/login/login.component.ts
+++ todo-app/src/app/components/login/login.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,RouterModule],
+  imports: [FormsModule, RouterModule, CommonModule],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -26,5 +27,18 @@ export class LoginComponent {
     } else {
       alert('Invalid email or password');
     }
+  }
+
+  emailInvalid(): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return this.email !== '' && !emailPattern.test(this.email);
+  }
+
+  emailInvalidOrEmpty(): boolean {
+    return this.emailInvalid() || this.email === '';
+  }
+
+  isFormValid(): boolean {
+    return !this.emailInvalidOrEmpty() && this.password !== '';
   }
 }
