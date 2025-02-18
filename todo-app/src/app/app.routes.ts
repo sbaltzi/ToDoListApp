82fb43e3ac6c677aade38cd4c2bbd99a92a26cbd
sbaltzi <60263769+sbaltzi@users.noreply.github.com>
2025-02-19 00:39:21 +0200

    Update routes

--- todo-app/src/app/app.routes.ts
+++ todo-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+// import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'to-do-list', component: ToDoListComponent/*, canActivate: [AuthGuard]*/ },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
+];
+
+export const appRouter = provideRouter(routes);
