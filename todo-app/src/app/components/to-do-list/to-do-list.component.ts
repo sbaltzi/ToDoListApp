87d38c937ca3161833c16624fdd3542a8ed0c5ed
sbaltzi <60263769+sbaltzi@users.noreply.github.com>
2025-02-18 13:11:34 +0200

    Add toDoList page

---
+++ todo-app/src/app/components/to-do-list/to-do-list.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-to-do-list',
+  imports: [FormsModule, CommonModule],
+  standalone: true,
+  templateUrl: './to-do-list.component.html',
+  styleUrl: './to-do-list.component.css'
+})
+export class ToDoListComponent implements OnInit {
+  taskTitle: string = '';
+  taskDescription: string = '';
+  tasks: any[] = [];
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.loadTasks();
+  }
+
+  addTask() {
+    if (!this.taskTitle.trim()) {
+      alert('Task title cannot be empty.');
+      return;
+    }
+
+    const newTask = {
+      id: Date.now(),
+      title: this.taskTitle,
+      description: this.taskDescription,
+      completed: false,
+    };
+
+    this.tasks.unshift(newTask);
+    this.saveTasks();
+    this.taskTitle = '';
+    this.taskDescription = '';
+  }
+
+  toggleTaskCompletion(task: any) {
+    task.completed = !task.completed;
+    this.saveTasks();
+  }
+
+  deleteTask(taskId: number) {
+    this.tasks = this.tasks.filter(task => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  loadTasks() {
+    this.tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.router.navigate(['/login']);
+  }
+}
