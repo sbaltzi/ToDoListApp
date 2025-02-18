45959c58c7d4c3db6d82bd4a329b7ffcbc7ff68c
sbaltzi <60263769+sbaltzi@users.noreply.github.com>
2025-02-18 17:29:18 +0200

    Add asterisk in markup for required fields and validators

--- todo-app/src/app/components/register/register.component.ts
+++ todo-app/src/app/components/register/register.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-  imports: [FormsModule, RouterModule],
+  imports: [FormsModule, RouterModule, CommonModule],
 })
 export class RegisterComponent {
   //todo: username
@@ -45,5 +46,18 @@ export class RegisterComponent {
 
     alert('Registration successful! Please login.');
     this.router.navigate(['/login']);
+  }
+
+  emailInvalid(): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return this.email !== '' && !emailPattern.test(this.email);
+  }
+
+  passwordInvalid(): boolean {
+    return this.password !== '' && this.password.length < 8;
+  }
+
+  confirmPasswordInvalid(): boolean {
+    return this.confirmPassword !== '' && this.password !== this.confirmPassword;
   }
 }
