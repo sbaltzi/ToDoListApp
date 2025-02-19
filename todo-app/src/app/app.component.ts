156dc3e67da078dfc78fe43967616b89ae9c6a15
sbaltzi <60263769+sbaltzi@users.noreply.github.com>
2025-02-19 14:33:12 +0200

    Add footer and don't include it in todo list page

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todo-app';
+  showFooter = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.showFooter = this.router.url !== '/to-do-list'; // Hide footer on login page
+    });
+  }
 }
